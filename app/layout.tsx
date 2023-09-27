a6327262a8f30a5ec4b07f69baed8f059a341877
Saurav Kumar <maxsaurav237@gmail.com>
2023-09-27 10:07:47 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-700 max-w-md m-auto font-sans">{children}</body>
     </html>
   );
 }
